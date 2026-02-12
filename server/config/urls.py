53b1271db74b0af1bc1c682b9970ade51d464bde
GryffinX <g.ayush2k07@gmail.com>
2026-02-12 20:31:08 +0530

    login and signup routes creation

--- server/config/urls.py
+++ server/config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('accounts.urls')),
+    path('api/auth/login/', TokenObtainPairView.as_view()),
+    path('api/auth/refresh/', TokenRefreshView.as_view()),
     path('api/', include('insurance.urls')),
     path('api/', include ('claims.urls')),
 ]
