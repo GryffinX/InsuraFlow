ed2de4b18323a4babd02ab41075b13b6d9a8b24a
GryffinX <g.ayush2k07@gmail.com>
2026-02-12 23:31:42 +0530

    OpenAPI, swagger and redoc setup

--- server/config/urls.py
+++ server/config/urls.py
@@ -17,6 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="InsuraFlow API",
+        default_version="v1",
+        description="Digital Insurance & Claims Processing API",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +39,6 @@ urlpatterns = [
     path('api/auth/refresh/', TokenRefreshView.as_view()),
     path('api/', include('insurance.urls')),
     path('api/', include ('claims.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
